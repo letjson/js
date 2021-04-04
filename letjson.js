11f93cfa2b74e546e096284efa9f7e8bb819884e
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2021-04-04 17:51:21 +0200

    Update letjson.js

--- letjson.js
+++ letjson.js
@@ -1,4 +1,79 @@
-loadJson = function (url) {
+
+/**
+ *
+ * @param url
+ * @param success
+ * @param error
+ * @returns {html|boolean}
+ */
+function letjson(url, success, error) {
+    var f = 'loadJson';
+
+
+    if (typeof success !== 'function') {
+        success = function () {
+            jlogs(f, ' success ', "included");
+        }
+    }
+
+    if (typeof error !== 'function') {
+        error = function () {
+            jlogs(f, ' error ', "Page not found.");
+        }
+    }
+    jlogs(f, ' url ', url);
+
+    if (url.length > 5) {
+
+        /* Make an HTTP request using the attribute value as the url name: */
+        var xhrObj = getXHRObject();
+        // xhrObj.setRequestHeader("Content-Type","text/html; charset=UTF-8");
+        // xhrObj.setRequestHeader("Content-Type","multipart/form-data; boundary=something");
+        xhrObj.onreadystatechange = function () {
+
+            if (this.readyState == 4) {
+                // document.onload =
+                loadJsonByStatus(this.status, this.responseText, url, success, error);
+
+                /* Remove the attribute, and call this function once more: */
+                // loadJson(url, success, error);
+            }
+        }
+        xhrObj.open("GET", url, true);
+        // xhrObj.responseType = 'text';
+        xhrObj.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        xhrObj.send();
+        /* Exit the function: */
+        return success(this);
+    }
+    return false;
+}
+
+
+/**
+ * @param status
+ * @param responseText
+ * @param url
+ * @param success
+ * @param error
+ * @returns {*}
+ */
+function loadJsonByStatus(status, responseText, url, success, error) {
+    var f = 'loadJsonByStatus';
+
+    if (status == 200) {
+        jlogs(f, ' loadJson loaded HTML: ', responseText);
+        return success(JSON.parse(responseText), url);
+    }
+    if (status == 404) {
+        getTarget(target).innerHTML = "loadJson Page not found.";
+        return error(this, status);
+    }
+    return error(responseText);
+}
+
+function loadJson2(url) {
         jlogs(this.constructor.name, ' self.cfg.target ', self.cfg.target);
 
         if (typeof url === 'object') {
