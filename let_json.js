633f93f14d4a2eaac37b8640f73d1fb5822d2db0
tom <tom@sapletta.com>
2021-05-02 19:39:38 +0200

    jlogs on the front

--- let_json.js
+++ let_json.js
@@ -1,6 +1,32 @@
 
 //jlogs - > jdebugs;
 
+// jlogs.js
+if (typeof jlogs !== 'function') jlogs = function () {
+    var str = ':: ';
+    for (var i in arguments) {
+        // console.log('--- jlogs', typeof arguments[i]);
+
+        if (typeof arguments[i] === "undefined") {
+            str += '';
+        } else if (typeof arguments[i] === "boolean") {
+            str += arguments[i];
+        } else if (typeof arguments[i] === "number") {
+            str += arguments[i];
+        } else if (typeof arguments[i] === "string") {
+            str += arguments[i];
+            // str += arguments[i].innerHTML;
+        } else if (typeof arguments[i] === "object") {
+            str += JSON.stringify(arguments[i]);
+        } else {
+            str += xml2string(arguments[i]);
+        }
+        str += ', ';
+    }
+    console.log(str);
+    return str;
+}
+
 /**
  *
  * @param url
@@ -134,31 +160,6 @@ function loadJson2(url) {
         return self;
 };
 
-// jlogs.js
-if (typeof jlogs !== 'function') jlogs = function () {
-    var str = ':: ';
-    for (var i in arguments) {
-        // console.log('--- jlogs', typeof arguments[i]);
-
-        if (typeof arguments[i] === "undefined") {
-            str += '';
-        } else if (typeof arguments[i] === "boolean") {
-            str += arguments[i];
-        } else if (typeof arguments[i] === "number") {
-            str += arguments[i];
-        } else if (typeof arguments[i] === "string") {
-            str += arguments[i];
-            // str += arguments[i].innerHTML;
-        } else if (typeof arguments[i] === "object") {
-            str += JSON.stringify(arguments[i]);
-        } else {
-            str += xml2string(arguments[i]);
-        }
-        str += ', ';
-    }
-    console.log(str);
-    return str;
-}
 
 
 if (typeof err !== 'function') err = function () {
