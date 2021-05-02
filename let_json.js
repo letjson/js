052dd94a0bfade6a9c5ad3c9fcf34c1e98869493
tom <tom@sapletta.com>
2021-05-02 19:34:47 +0200

    created function getXHRObject

--- let_json.js
+++ let_json.js
@@ -52,6 +52,31 @@ function let_json(url, success, error) {
     return false;
 }
 
+// xhr.js
+jlogs('exist?','getXHRObject');
+/**
+ * @returns {boolean}
+ */
+function getXHRObject() {
+    var xhrObj = false;
+    try {
+        xhrObj = new XMLHttpRequest();
+    } catch (e) {
+        var progid = ['MSXML2.XMLHTTP.5.0', 'MSXML2.XMLHTTP.4.0',
+            'MSXML2.XMLHTTP.3.0', 'MSXML2.XMLHTTP', 'Microsoft.XMLHTTP'];
+        for (var i = 0; i < progid.length; ++i) {
+            try {
+                xhrObj = new ActiveXObject(progid[i]);
+            } catch (e) {
+                continue;
+            }
+            break;
+        }
+    } finally {
+
+        return xhrObj;
+    }
+}
 
 /**
  * @param status
